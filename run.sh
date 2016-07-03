6f9b0d17134154a0cc5ea5518150f76a97fc4842
Webberist <me@webberist.com>
2016-07-03 14:00:05 +1000

    added block page

--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ docker run --name nginx -d --restart=always \
 	-p 80:80/tcp -p 3128:3128/tcp \
 	--volume $GITDIR/etc/nginx.conf:/etc/nginx/nginx.conf \
 	--volume $GITDIR/etc/conf.d:/etc/nginx/conf.d \
+	--volume $GITDIR/block.ls:/var/www/block.ls \
 	--volume $CACHEDIR:/data/ \
 	nginx:alpine &&
 docker logs -f nginx
