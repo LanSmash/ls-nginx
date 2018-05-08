cad4f839d895f0b55393a325a59536a0f29247bd
Webberist <me@webberist.com>
2018-05-08 16:14:32 +1000

    added windows update caching

--- run.sh
+++ run.sh
@@ -11,10 +11,13 @@ docker pull nginx:alpine
 docker stop nginx
 docker rm nginx
 docker run --name nginx -d --restart=always \
-	-p 80:80/tcp -p 3128:3128/tcp \
+	--network=host \
 	--volume $GITDIR/etc/nginx.conf:/etc/nginx/nginx.conf \
 	--volume $GITDIR/etc/conf.d:/etc/nginx/conf.d \
 	--volume $GITDIR/block.ls:/var/www/block.ls \
 	--volume $CACHEDIR:/data/ \
 	nginx:alpine &&
 docker logs -f nginx
+
+#       -p 80:80/tcp -p 3128:3128/tcp \
+
